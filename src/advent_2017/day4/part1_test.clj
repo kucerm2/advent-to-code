2f0782981e906ee57e197d04bbb9c74dc3a35d7f
Martin Kučera <uranic.market@gmail.com>
2017-12-04 21:14:00 +0100

    day 4

--- src/advent_2017/day4/part1_test.clj
+++ src/advent_2017/day4/part1_test.clj
@@ -1,5 +1,5 @@
 (ns advent-2017.day4.part1-test
-  (:require [clojure.test :refer [deftest are]]
+  (:require [clojure.test :refer [deftest are is]]
             [advent-2017.day4.part1 :refer [check-phrase solve]]))
 
 
@@ -8,3 +8,6 @@
              true "aa bb cc dd ee"
              false "aa bb cc dd aa"
              true "aa bb cc dd aaa"))
+
+(deftest test-solution
+  (is (= 337 (solve advent-2017.day4.data1/input))))
--- src/advent_2017/day4/part2.clj
+++ src/advent_2017/day4/part2.clj
@@ -1,10 +1,11 @@
-(ns advent-2017.day4.part1
+(ns advent-2017.day4.part2
   (:require [advent-2017.day4.data1 :as data]
             [clojure.string :as str]))
 
 (defn check-phrase [str]
   (some? (->>
           (str/split str #" ")
+          (map sort)
           (reduce
             (fn [used-set key]
               (if-not (contains? used-set key)
